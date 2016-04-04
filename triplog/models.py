493f3d582b17e3def4c1e7674412149354a04245
alextryonpdx <alextryonpdx@gmail.com>
2016-04-04 15:16:53 -0700

    water tracker initial commit

---
+++ triplog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Entry(models.Model):
+	employee = models.ForeignKey('auth.User')
+	editDate = models.DateTimeField(
+            default=timezone.now)
+	date = models.DateTimeField(
+		blank=True, null=False)
+	waterLevel = models.CharField(max_length=50)
+
+
+	def post(self):
+		self.editDate = timezone.now()
+		self.save
+
+	def __str__(self):
+		return self.employee.username

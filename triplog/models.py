344b34d59133006e7c9a2d98d01064cc0decbb49
alextryonpdx <alextryonpdx@gmail.com>
2016-04-07 11:46:30 -0700

    template working. develop blocks etc.

--- triplog/models.py
+++ triplog/models.py
@@ -4,18 +4,18 @@ from django import forms
 
 # Create your models here.
 class Entry(models.Model):
-	employee = models.ForeignKey('guide')
+	guide = models.ForeignKey('guide')
 	editDate = models.DateTimeField(
             default=timezone.now)
 	date = models.DateTimeField(
 		blank=True, null=False)
-	WHITE_SALMON_HALF = 'WSH'
-	WHITE_SALMON_FULL = 'WSF'
-	WIND = 'WND'
-	KLICKITAT = 'KCT'
-	HOOD = 'HDR'
-	FARMLANDS = 'FRM'
-	TIETON = 'TTN'
+	WHITE_SALMON_HALF = 'White Salmon Half-Day'
+	WHITE_SALMON_FULL = 'White Salmon Full-Day'
+	WIND = 'Wind River'
+	KLICKITAT = 'Klickitat River'
+	HOOD = 'Hood River'
+	FARMLANDS = 'The Farmlands'
+	TIETON = 'Tieton River'
 	River_Choices = (
 		(WHITE_SALMON_HALF, 'White Salmon Half-Day'),
 		(WHITE_SALMON_FULL, 'White Salmon Full-Day'),
@@ -25,31 +25,37 @@ class Entry(models.Model):
 		(FARMLANDS, 'The Farmlands'),
 		(TIETON, 'Tieton River')
 	)
-	river = models.CharField(max_length=3,
+	river = models.CharField(max_length=64,
 							choices=River_Choices,
 							null=True)
 
-	KAYAK = 'KYK',
-	RAFT = 'RFT'
+	KAYAK = 'Kayak'
+	RAFT = 'Raft'
 	TRIP_TYPE_CHOICES = (
 		(KAYAK, 'Kayak'),
 		(RAFT, 'Raft')
 	)
-	trip_type = models.CharField(max_length=3,
-								choices=TRIP_TYPE_CHOICES,
+	trip_type = models.CharField(max_length=64,
+							choices=TRIP_TYPE_CHOICES,
 								null=True)
 	leader = models.BooleanField(default=False)
-	rafts = models.IntegerField(null=True)
-	kayaks = models.IntegerField(null=True)
+	rafts = models.IntegerField(null=True, 
+								blank=True)
+	kayaks = models.IntegerField(null=True, 
+								blank=True)
 	waterLevel = models.CharField(max_length=50)
 	weather = models.TextField(max_length=356,
-								null=True)
+								null=True,
+								blank=True)
 	general_description = models.TextField(max_length=356,
-								null=True)
+								null=True,
+								blank=True)
 	problems = models.TextField(max_length=356,
-								null=True)
+								null=True,
+								blank=True)
 	guest_problems = models.TextField(max_length=356,
-								null=True)
+								null=True,
+								blank=True)
 	
 
 
@@ -64,7 +70,9 @@ class Entry(models.Model):
 
 
 class Guide(models.Model):
-	name = models.CharField(max_length=64)
+	FirstName = models.CharField(max_length=64)
+	LastName = models.CharField(max_length=64)
+
 	FULL_TIME = 'FT'
 	PART_TIME = 'PT'
 	ON_CALL = 'OC'
@@ -77,7 +85,7 @@ class Guide(models.Model):
 									choices=EMPLOYMENT_CHOICES)
 
 	def __str__(self):
-		return self.name
+		return self.LastName
 
 
 
--- triplog/views.py
+++ triplog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Entry
+from .models import Entry, Guide
 
 
 
 def entries_list(request):
-	entryList = Entry.objects.all
-	return render(request, 'triplog/entries_list.html', {})
+	entryList = Entry.objects.all()
+	return render(request, 'triplog/entries_list.html', {'entryList':entryList})
